2ffb1acc11124696e3ba7105120dc97e757be746
Filip Bojanowski <s19962@pjwstk.edu.pl>
2022-10-20 13:44:32 +0200

    Generalised query, now only set code is needed. All set is downloaded, Added function taking url query to scryfall

--- SetCubeSpreadsheet.py
+++ SetCubeSpreadsheet.py
@@ -4,31 +4,35 @@ import time
 import requests
 
 
-def get_card_list(magic_set, rarity):
+def get_card_list(magic_set):
 
-    request = requests.get('https://api.scryfall.com/cards/search?order=name&q=set:'+magic_set
-                           + '+rarity:'+rarity
-                           + '+is:booster'
-                           + "&unique=prints")
+    get_query_from_scryfall('https://api.scryfall.com/cards/search?order=rarity&q=set:'+magic_set +
+                            '+is:booster' +
+                            "&unique=prints")
+
+
+def get_query_from_scryfall(url):
+    request = requests.get(url)
     response = request.json()
 
     for card in response["data"]:
         card_needed = {
             'Name': card["name"],
-            'Rarity': rarity.upper(),
+            'Rarity': card["rarity"][0].upper(),
             'Color': "",
             'In Cube': 0,
             'Needed': 0,
             'Left': 0
         }
+        rarity = card["rarity"][0]
         for color in card["colors"]:
             card_needed["Color"] += color
-        # # For Identifying Attractions, not needed in not Unfinity
-        # if "attraction_lights" in card.keys():
-        #     card_needed["Attraction lights"] = ""
-        #     for value in card["attraction_lights"]:
-        #         card_needed["Attraction lights"] += str(value) + "|"
-        #     card_needed["Attraction lights"] = card_needed["Attraction lights"][:-1]
+        # For Identifying Attractions, not needed in not Unfinity
+        if "attraction_lights" in card.keys():
+            card_needed["Attraction lights"] = ""
+            for value in card["attraction_lights"]:
+                card_needed["Attraction lights"] += str(value) + "|"
+            card_needed["Attraction lights"] = card_needed["Attraction lights"][:-1]
 
         if rarity == 'r' or rarity == 'm':
             card_needed['Needed'] = 1
@@ -38,6 +42,9 @@ def get_card_list(magic_set, rarity):
             card_needed['Needed'] = 3
 
         cards_in_cube.append(card_needed)
+    if response["has_more"]:
+        time.sleep(0.5)
+        get_query_from_scryfall(response["next_page"])
 
 
 if __name__ == '__main__':
@@ -46,14 +53,7 @@ if __name__ == '__main__':
     magic_set = "unf"
 
     file = open("cube.json", "a")
-    get_card_list(magic_set, "m")
-    time.sleep(0.5)
-    get_card_list(magic_set, "r")
-    time.sleep(0.5)
-    get_card_list(magic_set, "u")
-    time.sleep(0.5)
-    get_card_list(magic_set, "c")
-    time.sleep(0.5)
+    get_card_list(magic_set)
     file.write(json.dumps(cards_in_cube))
 
 
